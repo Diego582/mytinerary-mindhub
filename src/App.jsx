12add68e891b1187831057bc99f1c0530ddc06c0
Diego Peña <penaomardiego@gmail.com>
2023-08-02 17:36:49 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { Box } from "@mui/material";
-import { MainLayout } from "@mui/material";
 import MainLayout from "./layouts/MainLayout";
+import Cities from "./pages/Cities";
 
 function App() {
   return (
     <MainLayout>
-      {/* <Home /> */}
-      <Cities />
+       <Home /> 
+      {/* <Cities /> */}
     </MainLayout>
   );
 }
